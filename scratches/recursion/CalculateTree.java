f14c97a457febfe430ad5172c2b9565eee85c0b8
12 <12>
2021-01-17 00:24:48 +0600

    added iterative calculatation of tree nodes sum

--- scratches/recursion/CalculateTree.java
+++ scratches/recursion/CalculateTree.java
@@ -2,9 +2,11 @@ package recursion;
 
 import recursion.tree.Tree;
 
-public class FillTree {
+public class CalculateTree {
     public static void main(String[] args) {
         Tree tree = new Tree(0, new Tree(1, new Tree(3), new Tree(4)), new Tree(2, new Tree(5), new Tree(6)));
-        System.out.println(tree.sum());
+        System.out.println("recursive sum: " + tree.recursiveSum());
+        System.out.println("iterative sum: " + tree.iterativeSum());
+
     }
 }
--- scratches/recursion/tree/Tree.java
+++ scratches/recursion/tree/Tree.java
@@ -1,5 +1,7 @@
 package recursion.tree;
 
+import recursion.datastructure.SimpleStack;
+
 public class Tree {
     private int value;
     private Tree left;
@@ -16,14 +18,31 @@ public class Tree {
         this.value = value;
     }
 
-    public int sum() {
-
+    public int recursiveSum() {
         if (left != null) {
-            s += left.sum();
+            s += left.recursiveSum();
         }
         if (right != null) {
-            s += right.sum();
+            s += right.recursiveSum();
         }
         return value + s;
+    }
+
+    public int iterativeSum() {
+        SimpleStack<Tree> stack = new SimpleStack<>();
+        int s = value;
+        stack.push(this);
+        while (!stack.isEmpty()) {
+            Tree node = stack.pop();
+            if (node.left != null) {
+                s += node.left.value;
+                stack.push(node.left);
+            }
+            if (node.right != null) {
+                s += node.right.value;
+                stack.push(node.right);
+            }
+        }
+        return s;
     }
 }
